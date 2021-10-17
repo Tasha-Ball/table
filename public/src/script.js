33c1d3048c2bd28d82387bd624feb83329e0cbfe
Baltikova <bnp@it-expertise.ru>
2021-10-18 00:10:58 +0500

    added resize

--- public/src/script.js
+++ public/src/script.js
@@ -14,6 +14,8 @@ const next = document.getElementById('next');
 const prev = document.getElementById('prev');
 const preloader = document.getElementById('preloader');
 const control = document.getElementById('control');
+let draggableW, draggableH= false
+let dragTarget, dragTargetWidth, clicX, width = null
 
 // рендеринг таблицы
 function render(data) {
@@ -29,12 +31,29 @@ function render(data) {
     let tr = document.createElement('tr');
     headers.forEach((head) => {
         let th = document.createElement('th');
-        th.onclick = () => ( sort(head.field, data));
+        th.onclick = (event) => {
+            event.preventDefault();
+            sort(head.field, data);
+        };
         th.innerHTML = head.title;
 
         // роллер вертикальный (для ресайза)
         let rollerVertical = document.createElement('span');
         rollerVertical.classList.add('roller-vertical');
+        rollerVertical.onmousedown = (event) => {
+            event.preventDefault();
+            draggableW = true
+            document.body.style.cursor = "w-resize";
+            dragTarget = event.target.parentNode;
+            dragTargetWidth = dragTarget.clientWidth;
+            clicX = event.x;
+        }
+        th.onmousemove = (event) => {
+            if ( draggableW && dragTargetWidth + event.x - clicX > 50) {
+                width = event.x - clicX
+                resizeTableWidth(dragTarget, dragTargetWidth, width)
+            }
+        }
         th.appendChild(rollerVertical);
 
         tr.appendChild(th);
@@ -42,6 +61,20 @@ function render(data) {
     // роллер горизонтальный (для ресайза)
     let rollerHorizontal = document.createElement('span');
     rollerHorizontal.classList.add('roller-horizontal');
+    rollerHorizontal.onmousedown = (event) => {
+        event.preventDefault();
+        draggableH = true
+        document.body.style.cursor = "w-resize";
+        dragTarget = event.target.parentNode;
+        dragTargetWidth = dragTarget.clientHeight;
+        clicX = event.y;
+    }
+    tr.onmousemove = (event) => {
+        if ( draggableH && dragTargetWidth + event.y - clicX > 5) {
+            width = event.y - clicX
+            dragTarget.style.height = `${dragTargetWidth + width}px`
+        }
+    }
     tr.appendChild(rollerHorizontal);
 
     thead.appendChild(tr);
@@ -56,20 +89,37 @@ function render(data) {
             // роллер вертикальный (для ресайза)
             let rollerVertical = document.createElement('span');
             rollerVertical.classList.add('roller-vertical');
+            rollerVertical.onmousedown = (event) => {
+                event.preventDefault();
+                draggableW = true
+                document.body.style.cursor = "w-resize";
+                dragTarget = event.target.parentNode;
+                dragTargetWidth = dragTarget.clientWidth;
+                clicX = event.x;
+            }
+            td.onmousemove = (event) => {
+                if ( draggableW && dragTargetWidth + event.x - clicX > 50) {
+                    width = event.x - clicX
+                    resizeTableWidth(dragTarget, dragTargetWidth, width)
+                }
+            }
             td.appendChild(rollerVertical);
 
-            // контексное меню (клик правой кнопкой)
+            // контекстное меню (клик правой кнопкой)
             let contextMenu = document.createElement('div');
             contextMenu.classList.add('context-menu')
             contextMenu.innerHTML = "Удалить"
-            contextMenu.onclick = () => ( removeItem(el.field, item, data));
+            contextMenu.onclick = (event) => {
+                event.preventDefault();
+                removeItem(el.field, item, data)
+            };
             td.appendChild(contextMenu);
-            td.oncontextmenu = (e) => {
-                e.preventDefault();
+            td.oncontextmenu = (event) => {
+                event.preventDefault();
                 if (document.getElementById('check')) document.getElementById('check').removeAttribute('id')
                 contextMenu.setAttribute('id', 'check')
-                contextMenu.style.top = e.offsetY + 'px'
-                contextMenu.style.left = e.offsetX + 'px'
+                contextMenu.style.top = event.offsetY + 'px'
+                contextMenu.style.left = event.offsetX + 'px'
             };
 
             tr.appendChild(td);
@@ -77,6 +127,20 @@ function render(data) {
         // роллер горизонтальный (для ресайза)
         let rollerHorizontal = document.createElement('span');
         rollerHorizontal.classList.add('roller-horizontal');
+        rollerHorizontal.onmousedown = (event) => {
+            event.preventDefault();
+            draggableH = true
+            document.body.style.cursor = "w-resize";
+            dragTarget = event.target.parentNode;
+            dragTargetWidth = dragTarget.clientHeight;
+            clicX = event.y;
+        }
+        tr.onmousemove = (event) => {
+            if ( draggableH && dragTargetWidth + event.y - clicX > 5) {
+                width = event.y - clicX
+                dragTarget.style.height = `${dragTargetWidth + width}px`
+            }
+        }
         tr.appendChild(rollerHorizontal);
 
         tbody.appendChild(tr);
@@ -88,7 +152,7 @@ function render(data) {
     noInfo.style.display = 'none';
     info.appendChild(table);
     control.style.display = 'flex';
-    // resizeTable();
+    setTableSize();
 };
 
 // запрос на сервер
@@ -152,11 +216,89 @@ function changePage(data) {
     }
 }
 
-// открытие контексного меню (удалить значение таблицы)
+// событие мыши (для ресайза)
+document.onmouseup = () => {
+    if (draggableW) {
+        resizeTableWidth(dragTarget, dragTargetWidth, width)
+        let widthColumn = []
+        document.querySelectorAll('th').forEach((th) => {
+            widthColumn.push(th.clientWidth)
+        })
+        sessionStorage.setItem('widthColumn', JSON.stringify(widthColumn))
+        document.body.style.cursor = "default"
+        dragTarget, dragTargetWidth, clicX, width = null
+        draggableW = false
+    }
+    if (draggableH) {
+        dragTarget.style.height = `${dragTargetWidth + width}px`
+        let heightColumn = []
+        document.querySelectorAll('tr').forEach((tr) => {
+            heightColumn.push(tr.clientHeight)
+        })
+        sessionStorage.setItem('heightColumn', JSON.stringify(heightColumn))
+        document.body.style.cursor = "default"
+        dragTarget, dragTargetWidth, clicX, width = null
+        draggableH = false
+    }
+}
+
+// изменение ширины столбцов
+function resizeTableWidth(dragTarget, dragTargetWidth, width) {
+    const tds = document.querySelectorAll(`td:nth-child(${dragTarget.cellIndex+1}),th:nth-child(${dragTarget.cellIndex+1})`)
+    if ( tds ){
+        tds.forEach( (td) => {
+            td.style.minWidth = `${dragTargetWidth + width}px`
+            td.style.maxWidth = `${dragTargetWidth + width}px`
+        })
+    }
+}
+
+// ихменение ширины и/или высоты столбцов/строк
+function setTableSize() {
+    const widthColumn = JSON.parse(sessionStorage.getItem('widthColumn'))
+    const heightColumn = JSON.parse(sessionStorage.getItem('heightColumn'))
+
+    if (widthColumn) {
+        for (let ind = 0; ind < widthColumn.length; ind++) {
+            const tds = document.querySelectorAll(`td:nth-child(${ind+1}),th:nth-child(${ind+1})`)
+            tds.forEach( (td) => {
+                td.style.minWidth = `${widthColumn[ind]}px`
+                td.style.maxWidth = `${widthColumn[ind]}px`
+            })
+        }
+    } else {
+        const widthTable = document.getElementById('table').clientWidth
+        const width = widthTable / headers.length
+        const ths = document.querySelectorAll("th,td")
+        ths.forEach( (el) => {
+            el.style.minWidth = `${width}px`
+            el.style.maxWidth = `${width}px`
+        })
+    }
+
+    if (heightColumn) {
+        for (let ind = 1; ind < heightColumn.length; ind++) {
+            const trs = document.querySelectorAll(`tr:nth-child(${ind})`)
+            trs.forEach( (tr) => {
+                tr.parentNode.nodeName==='THEAD'
+                    ? tr.style.height = `${heightColumn[0]}px`
+                    : tr.style.height = `${heightColumn[ind]}px`
+            })
+        }
+    }
+
+}
+
+// открытие контекстного меню (удалить значение таблицы)
 function removeItem(el, item, data) {
     const index = data[0].results.indexOf(item);
     data[0].results[index][el] = "";
     render(data);
+}
+
+// закрытие контекстного меню
+document.onclick = (e) => {
+    if (document.getElementById('check')) document.getElementById('check').removeAttribute('id')
 }
 
 // механизм отображения данных при перезагрузке страницы
@@ -167,8 +309,4 @@ window.onload = function() {
         changePage(data);
         render(data)
     }
-}
-
-document.onclick = (e) => {
-    if (document.getElementById('check')) document.getElementById('check').removeAttribute('id')
 }
